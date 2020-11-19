8600907f61420904d87577b504e055ff59a7b0cf
Lucas <lucascpinheiro10@gmail.com>
2020-11-18 21:32:13 -0300

    Setup jest environment and implement tests for routes

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ Start server as production
 
 Run tests
 
+## guides to implement tests
+
+- https://dev.to/nedsoft/testing-nodejs-express-api-with-jest-and-supertest-1km6
+
+- https://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/
+
+- https://medium.com/@pojotorshemi/integration-test-on-express-restful-apis-using-jest-and-supertest-4cf5d1414ab0
---
+++ tests/routes.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+describe('Routes', () => {
+  it('should create new short url', async () => {
+    const res = await request(app)
+      .post('/')
+      .send({
+        url: 'www.xyz.com',
+        date: 'month-day-year'
+      })
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toHaveProperty('msg')
+  })
+
+  it('should create new post', async () => {
+    const res = await request(app).get('/')
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toHaveProperty('msg')
+  })
+})
